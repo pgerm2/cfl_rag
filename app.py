65fe687eec1eba3dee2dd8001290c59cf5889460
Phillip Germany II <59968773+pgerm2@users.noreply.github.com>
2025-06-13 21:05:37 -0400

    Update1 app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def logout():
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000)) # Heroku assigns a port via PORT environment variable
     # app.run(debug=True, host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=10000)
